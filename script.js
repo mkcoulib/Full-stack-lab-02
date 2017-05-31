14b0b72d6650c1a730ea5714d228f2fba3ed0ac0
mkcoulib <mkcoulib@gmail.com>
2017-05-31 16:10:10 -0400

    modified the loop to include an if else condition

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ var friends  = ['Mike', 'Alexis', 'Moussa', 'Veronica','Jason'];
 
 for (var i = 0; i < friends.length; i++) {
     console.log (friends[i]);
-    for (var j = 99; j>1; j--){
+    for (var j = 99; j>=1; j--){
         var x = j-1;
-        console.log(j + " lines of code in the file, " + j+ " lines of code; " + friends[i] + " strikes one out, clears it all out, "+ x +" lines of code in the file" );
-    }
-    console.log( "1 line of code in the file, 1 line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file" );
+        if (j>2){
+            console.log(j + " lines of code in the file, " + j+ " lines of code; " + friends[i] + " strikes one out, clears it all out, "+ x +" lines of code in the file" );
+    } else {
+       console.log( j +" line of code in the file, 1 line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file" );
 }
+    };
+};
 
 
 
